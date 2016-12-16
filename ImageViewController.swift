40128c55edfbeeff85ed6d2b7b9d16ea0b1df8ac
deniz <denisdarm@gmail.com>
2016-12-16 15:37:48 +0200

    first commit, organized ui and controller. now working on photos

---
+++ ImageViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ImageViewController.swift
+//  MathPh
+//
+//  Created by Denis Dobynda on 12/16/16.
+//  Copyright © 2016 Denis Dobynda. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate
+{
+
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            if let imageData = NSData(contentsOf: url as URL) {
+                image = UIImage(data: imageData as Data)
+            }
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView!{
+        didSet {
+            scrollView.contentSize = imageView.frame.size
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.08
+            scrollView.maximumZoomScale = 1.0
+            scrollView.setZoomScale(0.08, animated: true)
+        }
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.addSubview(imageView)
+    }
+    
+}
